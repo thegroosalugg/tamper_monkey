333dca07384df28026acf20176353fff6b32b1f4
victor loginov <tkoe88@zoho.com>
2025-09-27 11:45:11 +0200

    update urls again

--- indeed/indeed.meta.js
+++ indeed/indeed.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Indeed Enhanced
 // @namespace    http://tampermonkey.net/
-// @version      1.0.2
+// @version      1.0.3
 // @description  Add infinite scroll; filter search results;
 // @author       thegroosalugg
 // @match        https://*.indeed.com/*
@@ -9,4 +9,6 @@
 // @match        https://*.indeed.*/*
 // @run-at       document-end
 // @grant        none
+// @updateURL    https://raw.githubusercontent.com/thegroosalugg/tamper_monkey/main/indeed/indeed.meta.js
+// @downloadURL  https://raw.githubusercontent.com/thegroosalugg/tamper_monkey/main/indeed/indeed.user.js
 // ==/UserScript==
