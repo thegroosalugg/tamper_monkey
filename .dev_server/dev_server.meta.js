6f05d2068c3584d4022015b0aec4c52c11038bc9
victor loginov <tkoe88@zoho.com>
2025-09-27 12:01:50 +0200

    added script for development server

---
+++ .dev_server/dev_server.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name        Dev Server
+// @namespace   http://tampermonkey.net/
+// @version     0.0.1
+// @description Server for testing scripts. Configured to discard cache for instant feedback on refresh
+// @author      thegroosalugg
+// @match       *://*/*
+// @connect     localhost
+// @grant       GM_xmlhttpRequest
+// @updateURL   https://raw.githubusercontent.com/thegroosalugg/tamper_monkey/main/.dev_server/dev_server.meta.js
+// @downloadURL https://raw.githubusercontent.com/thegroosalugg/tamper_monkey/main/.dev_server/dev_server.user.js
+// ==/UserScript==
